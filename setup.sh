71680a53fd3c5835b12854000ed4908c4b6a34ac
zeekay <zeekayy@gmail.com>
2011-11-02 14:33:23 -0500

    Added support for external repositories

--- setup.sh
+++ setup.sh
@@ -16,3 +16,26 @@ for DOTFILE in $BASEDIR/dot.*; do
     echo linking $DOTNAME
     ln -s "$DOTFILE" "$DEST"
 done
+
+echo "list any additional external repos to install or enter to exit"
+read EXTERNAL_REPOS
+
+for REPO in $EXTERNAL_REPOS; do
+    echo "install dot files from $REPO? (y/n)"
+    read INPUT
+    if [ "$INPUT" == "y" ]; then
+        DEST=~/."$REPO"
+        if [ -d "$BASEDIR/dot-$REPO" ]; then
+            rm -rf $BASEDIR/dot-$REPO
+        fi
+        hg clone https://bitbucket.org/zeekay/dot-$REPO
+        if [ "$1" == "-f" ]; then
+            rm -rf "$DEST"
+        fi
+        echo linking $DEST
+        ln -s $BASEDIR/dot-$REPO $DEST
+        if [ -f "$DEST/setup.sh" ]; then
+            . $DEST/setup.sh
+        fi
+    fi
+done
