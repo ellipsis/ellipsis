d4fee41cc227e40ab9c096badccae013a2d752d9
zeekay <zeekayy@gmail.com>
2011-11-03 05:31:23 +0000

    Updated setup script

--- setup.sh
+++ setup.sh
@@ -24,18 +24,10 @@ for REPO in $EXTERNAL_REPOS; do
     echo -n "install dot files from $REPO? (y/n) "
     read INPUT
     if [ "$INPUT" == "y" ]; then
-        DEST=~/."$REPO"
         if [ -d "$BASEDIR/dot-$REPO" ]; then
             rm -rf $BASEDIR/dot-$REPO
         fi
-        hg clone https://bitbucket.org/zeekay/dot-$REPO
-        if [ "$1" == "-f" ]; then
-            rm -rf "$DEST"
-        fi
-        echo linking $DEST
-        ln -s $BASEDIR/dot-$REPO $DEST
-        if [ -f "$DEST/setup.sh" ]; then
-            . $DEST/setup.sh
-        fi
+        hg clone https://bitbucket.org/zeekay/dot-$REPO $BASEDIR/dot-$REPO
+        . $DEST/setup.sh
     fi
 done
