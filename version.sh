e1adbd6b749470049ef33af886ea4d0e83ba627b
zeekay <zeekayy@gmail.com>
2011-11-05 10:54:23 -0500

    Change version.sh format

--- version.sh
+++ version.sh
@@ -2,9 +2,10 @@
 
 basedir="$( cd -P "$( dirname "$0" )" && pwd )"
 
-echo dot-files:`hg id -n $basedir`
+echo dot-files
+hg id -n $basedir
 
 for dir in $basedir/dot-*; do
-    name=`echo $dir | rev | cut -d "/" -f1 | rev`
-    echo ${name:4}:`hg id -n $dir`
+    echo $dir | rev | cut -d "/" -f1 | rev
+    hg id -n $dir
 done
