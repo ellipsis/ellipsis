ebaad6c5d1d538bf151252c45c8382f05c285735
zeekay <zeekayy@gmail.com>
2011-12-31 08:00:19 -0600

    Update setup script

--- setup.sh
+++ setup.sh
@@ -39,32 +39,28 @@ for repo in $external_repos; do
     # reset basedir
     basedir=$dotfiles_dir
     echo
-    echo "clone and install $repo? (y/n) (default: n)"
-    read input
-    if [ "$input" = "y" ]; then
-        case $repo in
-            hg+*)
-                repo=`echo $repo | cut -c 4-`
-                name=`echo $repo | rev | cut -d "/" -f1 | rev`
-                # trim dot- from beginning of repo name
-                [ "`echo $name | cut -c1-4`" = "dot-" ] && name=`echo $name | cut -c5-`
-                hg clone $repo $basedir/$name
-            ;;
-            git+*)
-                repo=`echo $repo | cut -c 5-`
-                name=`echo $repo | rev | cut -d "/" -f1 | rev | cut -c5-`
-                # trim dot- from beginning of repo name
-                [ "`echo $name | cut -c1-4`" = "dot-" ] && name=`echo $name | cut -c5-`
-                git clone $repo $basedir/$name
-            ;;
-            *)
-                name=$repo
-                # trim dot- from beginning of repo name
-                [ "`echo $name | cut -c1-4`" = "dot-" ] && name=`echo $name | cut -c5-`
-                hg clone https://bitbucket.org/zeekay/dot-$name $basedir/$name
-            ;;
-        esac
-        repo_basedir=$basedir/$name
-        . $repo_basedir/setup.sh
-    fi
+    case $repo in
+        hg+*)
+            repo=`echo $repo | cut -c 4-`
+            name=`echo $repo | rev | cut -d "/" -f1 | rev`
+            # trim dot- from beginning of repo name
+            [ "`echo $name | cut -c1-4`" = "dot-" ] && name=`echo $name | cut -c5-`
+            hg clone $repo $basedir/$name
+        ;;
+        git+*)
+            repo=`echo $repo | cut -c 5-`
+            name=`echo $repo | rev | cut -d "/" -f1 | rev | cut -c5-`
+            # trim dot- from beginning of repo name
+            [ "`echo $name | cut -c1-4`" = "dot-" ] && name=`echo $name | cut -c5-`
+            git clone $repo $basedir/$name
+        ;;
+        *)
+            name=$repo
+            # trim dot- from beginning of repo name
+            [ "`echo $name | cut -c1-4`" = "dot-" ] && name=`echo $name | cut -c5-`
+            hg clone https://bitbucket.org/zeekay/dot-$name $basedir/$name
+        ;;
+    esac
+    repo_basedir=$basedir/$name
+    . $repo_basedir/setup.sh
 done
