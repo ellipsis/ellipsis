9bcd4b6952415da71c3d7d689472d204c762c779
zeekay <zeekayy@gmail.com>
2012-02-11 11:10:42 -0600

    xmonad-log-applet w/DBus 0.4

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -3,6 +3,10 @@
 
 import System.IO
 import System.Exit
+import Control.OldException
+import DBus
+import DBus.Connection
+import DBus.Message
 import XMonad
 import XMonad.Config.Gnome
 import XMonad.Util.Replace
@@ -159,10 +163,67 @@ myManageHook = composeAll
     , resource  =? "desktop_window" --> doIgnore
     , resource  =? "kdesktop"       --> doIgnore ]
 
-main = do
-    xmobar <- spawnPipe "xmobar"
+-- xmonad log applet setup for gnome-panel
+prettyPrinter :: Connection -> PP
+prettyPrinter dbus = defaultPP
+    { ppOutput   = dbusOutput dbus
+    , ppTitle    = pangoSanitize
+    , ppCurrent  = pangoColor "green" . wrap "[" "]" . pangoSanitize
+    , ppVisible  = pangoColor "yellow" . wrap "(" ")" . pangoSanitize
+    , ppHidden   = const ""
+    , ppUrgent   = pangoColor "red"
+    , ppLayout   = const ""
+    , ppSep      = " "
+    }
+
+getWellKnownName :: Connection -> IO ()
+getWellKnownName dbus = tryGetName `catchDyn` (\(DBus.Error _ _) -> getWellKnownName dbus)
+  where
+    tryGetName = do
+        namereq <- newMethodCall serviceDBus pathDBus interfaceDBus "RequestName"
+        addArgs namereq [String "org.xmonad.Log", Word32 5]
+        sendWithReplyAndBlock dbus namereq 0
+        return ()
+
+dbusOutput :: Connection -> String -> IO ()
+dbusOutput dbus str = do
+    msg <- newSignal "/org/xmonad/Log" "org.xmonad.Log" "Update"
+    addArgs msg [String ("<b>" ++ str ++ "</b>")]
+    -- If the send fails, ignore it.
+    send dbus msg 0 `catchDyn` (\(DBus.Error _ _) -> return 0)
+    return ()
+
+pangoColor :: String -> String -> String
+pangoColor fg = wrap left right
+  where
+    left  = "<span foreground=\"" ++ fg ++ "\">"
+    right = "</span>"
+
+pangoSanitize :: String -> String
+pangoSanitize = foldr sanitize ""
+  where
+    sanitize '>'  xs = "&gt;" ++ xs
+    sanitize '<'  xs = "&lt;" ++ xs
+    sanitize '\"' xs = "&quot;" ++ xs
+    sanitize '&'  xs = "&amp;" ++ xs
+    sanitize x    xs = x:xs
+
+-- Use xmobar
+-- main = do
+    -- xmobar <- spawnPipe "xmobar"
+    -- xmonad $ gnomeConfig {
+        -- logHook              = dynamicLogWithPP $ xmobarPP {
+        --     ppOutput = hPutStrLn xmobar
+        --     , ppTitle = xmobarColor "#FFB6B0" "" . shorten 100
+        --     , ppCurrent = xmobarColor "#CEFFAC" ""
+        --     , ppSep = "   "
+        -- }
+
+-- Use xmonad log applet
+main = withConnection Session $ \ dbus -> do
     xmonad $ gnomeConfig {
-          terminal           = myTerminal
+          logHook            = dynamicLogWithPP (prettyPrinter dbus)
+        , terminal           = myTerminal
         , focusFollowsMouse  = myFocusFollowsMouse
         , borderWidth        = myBorderWidth
         , modMask            = myModMask
@@ -171,12 +232,9 @@ main = do
         , mouseBindings      = myMouseBindings
         , layoutHook         = smartBorders $ myLayout
         , manageHook         = manageHook $ gnomeConfig
-        , logHook            = dynamicLogWithPP $ xmobarPP {
-            ppOutput = hPutStrLn xmobar
-            , ppTitle = xmobarColor "#FFB6B0" "" . shorten 100
-            , ppCurrent = xmobarColor "#CEFFAC" ""
-            , ppSep = "   "
-        }
         , startupHook        = do
             setWMName "LG3D"
+            -- spawn "nm-applet"
+            -- spawn "gnome-sound-applet"
+            -- spawn "trayer --edge top --align right --widthtype request --margin 120 --height 18 --padding 1 --transparent true --tint 0x000000 --alpha 0 &"
     }
