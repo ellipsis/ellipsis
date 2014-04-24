af26df2b84291c1e954cb6a36b797c0c1ebdec47
Zach Kelling <zk@monoid.io>
2014-04-24 13:45:17 -0500

    Don't blow up because of git config, more reliable way of getting path to where we're installed.

--- src/init.sh
+++ src/init.sh
@@ -4,21 +4,24 @@
 # Setup initial globals and load function used to source other modules.
 
 # These globals can be set by a user to use a custom ellipsis fork/set of packages/etc
-ELLIPSIS_USER="${ELLIPSIS_USER:-$(git config github.user)}"
-ELLIPSIS_REPO="${ELLIPSIS_REPO:-https://github.com/$ELLIPSIS_USER/ellipsis}"
-ELLIPSIS_PATH=${ELLIPSIS_PATH:-$HOME/.ellipsis}
+
+# git config returns 1 if key isn't found, so we pipe to cat to squash that in
+# the case it's not-defined.
+if [[ -z "$ELLIPSIS_USER" ]]; then
+    GITHUB_USER=$(git config github.user | cat)
+    ELLIPSIS_USER=${GITHUB_USER:-zeekay}
+fi
+ELLIPSIS_REPO=${ELLIPSIS_REPO:-https://github.com/$ELLIPSIS_USER/ellipsis}
+ELLIPSIS_PATH="$(cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd)"
 
 # Default package operation act upon is ellipsis.
 # These are highly mutable.
 PKG_PATH=${PKG_PATH:-$ELLIPSIS_PATH}
 PKG_NAME=${PKG_NAME:-${PKG_PATH##*/.}}
 
-# Determine where we are
-ELLIPSIS_SRC="${ELLIPSIS_SRC:-$(dirname "${BASH_SOURCE[0]}")}"
-
 # Source other src files easily, and only once!
 load() {
-    source $ELLIPSIS_SRC/$1.sh
+    source $ELLIPSIS_PATH/src/$1.sh
 }
 
 # Load version info.
