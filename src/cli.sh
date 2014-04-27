8989d5f30c42ce9f0fc42e1f57bb25758e017bd8
Zach Kelling <zk@monoid.io>
2014-04-26 22:17:17 -0500

    Update usage.

--- src/cli.sh
+++ src/cli.sh
@@ -26,15 +26,16 @@ Usage: ellipsis <command>
     -v, --version  show version
 
   Commands:
-    install        install new package
-    uninstall      uninstall package
-    list           list installed packages
-    links          list symlinks installed globally or for a package
-    available      list packages available for install
-    new            create a new package
-    pull           git pull all packages
-    push           git push all packages
-    status         show status of all packages
+    new        create a new package
+    install    install new package
+    uninstall  uninstall package
+    unlink     unlink package
+    list       list installed packages
+    links      show symlinks installed by package(s)
+    pull       git pull package(s)
+    push       git push package(s)
+    status     show status of package(s)
+    search     search package repository
 	EOF
 }
 
