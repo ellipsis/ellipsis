f7102d930b42958d9adf6ca8f2f06058cf5353fc
Zach Kelling <z@zeekay.io>
2014-05-02 22:59:26 -0500

    Add test fixture.

---
+++ test/fixtures/files/ellipsis.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+#
+# zeekay/test
+#
+# Test package.
+
+pkg.link() {
+    fs.link_files common
+
+    case $(os.platform) in
+        cygwin)
+            fs.link_files platform/cygwin
+            ;;
+        osx)
+            fs.link_files platform/osx
+            ;;
+        freebsd)
+            fs.link_files platform/freebsd
+            ;;
+        linux)
+            fs.link_files platform/linux
+            ;;
+    esac
+}
