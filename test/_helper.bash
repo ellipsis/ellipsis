e121d0aee4cea1168e0f1c55dad15e591cb1b667
Zach Kelling <zk@monoid.io>
2014-05-04 01:26:01 -0500

    Update test _helper.

--- test/_helper.bash
+++ test/_helper.bash
@@ -3,13 +3,9 @@
 # test/_helper.bash
 # Just a little helper file for bats.
 
-export TESTS_DIR="$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)"
-export PATH="$TESTS_DIR/../bin:$PATH"
+export TESTS_DIR="$BATS_TEST_DIRNAME"
+export ELLIPSIS_PATH="$(cd $TESTS_DIR/.. && pwd)"
+export PATH="$ELLIPSIS_PATH/bin:$PATH"
 
 # Initialize ellipsis, which replaces bat's `load` function with ours.
 load ../src/init
-
-# Install ourselves for Travis CI
-if [ "$TRAVIS" ] && [ ! -e "/home/travis/.ellipsis" ]; then
-    ln -s /home/travis/build/zeekay/ellipsis /home/travis/.ellipsis
-fi
