21f17474a0aad0f498efa80a12bf6d651c597ac3
G. Roggemans <g.roggemans@grog.be>
2016-07-03 20:45:47 +0200

    Placed global vars in own file
    By placing the global vars in there own file, it is no longer needed to
    source the load function for the env/init system. This also prevents
    non-POSIX code being executed by other shells.

--- src/init.bash
+++ src/init.bash
@@ -3,42 +3,15 @@
 # Setup initial globals and load function used to source other modules. This
 # file must be sourced before any others.
 
-# Count nested runs for msg indentation
-if [ -z "$ELLIPSIS_LVL" ]; then
-    export ELLIPSIS_LVL=1
-else
-    let ELLIPSIS_LVL=ELLIPSIS_LVL+1
-fi
-
-if [ -z "$ELLIPSIS_USER" ]; then
-    # Pipe to cat to squash git config's exit code 1 in case of missing key.
-    GITHUB_USER="$(git config github.user | cat)"
-    ELLIPSIS_USER="${GITHUB_USER:-${USERNAME:-$(whoami)}}"
-fi
-
-# Repostitory defaults.
-ELLIPSIS_PROTO="${ELLIPSIS_PROTO:-https}"
-ELLIPSIS_REPO="${ELLIPSIS_REPO:-https://github.com/$ELLIPSIS_USER/ellipsis}"
-
-# Default locations for ellipsis and packages.
-ELLIPSIS_HOME="${ELLIPSIS_HOME:-$HOME}"
-ELLIPSIS_PACKAGES="${ELLIPSIS_PACKAGES:-$ELLIPSIS_PATH/packages}"
-
-# Ellipsis is the default package.
-PKG_PATH="${PKG_PATH:-$ELLIPSIS_PATH}"
-PKG_NAME="${PKG_NAME:-${PKG_PATH##*/.}}"
-
 # Utility to load other modules. Uses a tiny bit of black magic to ensure each
 # module is only loaded once.
 load() {
-    # Set dynamic loaded variable name
+    # Use indirect expansion to reference dynamic variable which flags this
+    # module as loaded.
     local loaded="__loaded_$1"
 
-    # Get status of loaded variable
-    eval "local status=\"\$$loaded\""
-
     # Only source modules once
-    if [ -z "$status" ]; then
+    if [[ -z "${!loaded}" ]]; then
         # Mark this module as loaded, prevent infinite recursion, ya know...
         eval "$loaded=1"
 
@@ -51,5 +24,6 @@ load() {
     fi
 }
 
-# Load version info.
+# Load version info and global variables.
 load version
+load vars
